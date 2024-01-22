f5dddd8b3d726ce6df28b7368639541400de96c1
EvgeniyZo <evgeniizo@yandex.ru>
2024-01-22 13:11:04 +0300

    Initial commit

---
+++ linux_terminal/linux_commands.sh
@@ -0,0 +1,83 @@
+
+
+
+pwd
+ls
+cd
+
+mkdir
+touch
+
+cat
+echo
+
+rm
+rmdir
+
+cp
+mv
+ps
+
+scp
+
+top
+
+alias
+
+env
+
+history
+
+# users
+
+useradd
+userdel
+usermod
+passwd
+сhage
+сhfn
+chsh
+
+# groups
+
+groupadd
+groupdel
+groupmod
+gpasswd
+
+# permissions
+
+chmod
+chown
+
+# data about myself
+
+id
+whoami
+w
+groups
+
+# text tools
+diff
+patch
+head
+tail
+sort
+join
+tee
+split
+wc # word count
+iconv
+tr
+
+sed
+awk
+perl
+
+vi # стандартный текстовый редактор операционной системы UNIX
+vim # Vi IMproved
+
+emacs
+nano
+mcedit # part of midnight commander
+
---
+++ linux_terminal/tools.sh
@@ -0,0 +1,2 @@
+
+sudo sysctl -w net.ipv4.ip_default_ttl=65
